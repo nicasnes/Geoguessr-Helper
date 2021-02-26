24170623947b8060601eff8943eb7dea25cbe0ce
nicasnes <31024845+nicasnes@users.noreply.github.com>
2021-02-26 15:57:44 -0500

    Automate GET requests to avoid refresh requirement
    - Use setInterval function to ping the endpoint every 5 seconds, avoiding the need to refresh the page to retrieve the location at the expense of some memory
    - Remove the requirement for the round to be over to view the answer

--- content.js
+++ content.js
@@ -14,36 +14,36 @@ function getServerJSON(br) {
 function determineCoords(url) {
   let coords = {};
   fetch(url, {
-    credentials: 'include',
+    credentials: 'include'
   }).then(res=>res.json())
     .then(data => {
-      console.log('JSON:', data);
       let roundArr = data["rounds"];
       if (roundArr.length > 0) {
         let roundAns = roundArr[roundArr.length-1];
-        if (roundAns["answer"]) {
-          coords.lat = roundArr[roundArr.length-1].lat;
-          coords.lng = roundArr[roundArr.length-1].lng;
-          coords.panoId = roundArr[roundArr.length-1].panoId;
-          destURL = 'https://maps.google.com/?q=' + coords.lat + ',' + coords.lng;
-          chrome.storage.sync.set({urlKey: destURL}, function(result){
+        coords.lat = roundArr[roundArr.length-1].lat;
+        coords.lng = roundArr[roundArr.length-1].lng;
+        coords.panoId = roundArr[roundArr.length-1].panoId;
+        destURL = 'https://maps.google.com/?q=' + coords.lat + ',' + coords.lng;
+        chrome.storage.sync.set({urlKey: destURL}, function(result){
           });
-        }
       }
     })
   .catch(err => { throw err; });
   return coords;
 }
 
-function setURL() { 
-  console.log("Setting url to:" + destURL);
+function setURL() {
   chrome.storage.sync.get(['urlKey'], function(result) { 
     document.getElementById('link').setAttribute("href", result.urlKey);
   });
 }
 
-if (location.href.includes("geoguessr")) {
+function coordHandler() { 
   determineCoords(serverJSON);
+}
+
+if (location.href.includes("geoguessr")) {
+  setInterval(coordHandler, 5*1000);
 }
 
 if (location.href.includes("popup")) {
@@ -53,7 +53,6 @@ if (location.href.includes("popup")) {
 /**
  * 
  * TODO:
- * Handle error with undefined,undefined when no guesses made
  * Change from battle-royale/* to exclude battle-royale/ main page --- fixed, now fix on first load
  * Handle error with undefined,undefined after wrong guess
  * Make it so you don't have to refresh
