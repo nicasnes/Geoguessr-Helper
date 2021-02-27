93ef7114391dfec84bc01f28441a446a408251d2
nicasnes <31024845+nicasnes@users.noreply.github.com>
2021-02-26 20:51:17 -0500

    Refactor code and improve documentation

--- content.js
+++ content.js
@@ -1,59 +1,85 @@
-// Test URL: "https://game-server.geoguessr.com/api/battle-royale/a7b231e7-6985-4700-b737-ecf3812a74e6/";
+/**
+ * Geoguessr Helper Content Script
+ * Author: Nic Asnes
+ * Extension is open source at https://github.com/nicasnes/Geoguessr-Helper
+ */
 
-let serverJSON = getServerJSON(location.href);
-let destURL = ""
+let endpointURL = getEndpoint(location.href);
+let destURL = "";
+let interval = 30;
 
-/* Given a battle royale URL, extract the game id and return JSON from server */
-// getServerJSON(br: string): string
-function getServerJSON(br) {
+/**
+ * If in a Battle Royale game, run coordHandler every *interval* seconds.
+ */
+if (location.href.includes("geoguessr")) {
+  setInterval(coordHandler, interval*1000);
+}
+
+/**
+ * Run initPopup whenever popup.html is opened.
+ * Note the content script matches in manifest.json providing safety
+ */
+if (location.href.includes("popup")) {
+  initPopup(destURL);
+}
+
+/**
+* Given a battle royale URL, extract the game id and return JSON from server 
+*/
+function getEndpoint(br) {
   let id = br.substring(br.indexOf("royale/") + 7);
   return "https://game-server.geoguessr.com/api/battle-royale/" + id;
 }
 
-// determineLocation(url: string): Object{number, number, string}
+/**
+* Given a URL, make a GET request to the Geoguessr endpoint, storing the maps URL for the location
+* if the answer has been guessed. Returns the location's coordinates. 
+*/
 function determineCoords(url) {
   let coords = {};
+
+  // Make a GET request to the Geoguessr endpoint
   fetch(url, {
+    // Include credentials to GET from the endpoint
     credentials: 'include'
+
   }).then(res=>res.json())
     .then(data => {
       let roundArr = data["rounds"];
-      if (roundArr.length > 0) {
-        let roundAns = roundArr[roundArr.length-1];
-        coords.lat = roundArr[roundArr.length-1].lat;
-        coords.lng = roundArr[roundArr.length-1].lng;
-        coords.panoId = roundArr[roundArr.length-1].panoId;
-        destURL = 'https://maps.google.com/?q=' + coords.lat + ',' + coords.lng;
-        chrome.storage.sync.set({urlKey: destURL}, function(result){
-          });
+      
+      // If there has been a round
+      if (roundArr && roundArr.length > 0) {
+
+        // If the current round is over
+        if (roundArr[roundArr.length-1].answer) {
+
+          // Store the latitude and longitude in coords
+          coords.lat = roundArr[roundArr.length-1].lat;
+          coords.lng = roundArr[roundArr.length-1].lng;
+
+          // Store destURL in chrome's synced storage
+          destURL = 'https://maps.google.com/?q=' + coords.lat + ',' + coords.lng;
+          chrome.storage.sync.set({urlKey: destURL}, function(result){});
+        }
       }
     })
   .catch(err => { throw err; });
+  
   return coords;
 }
 
-function setURL() {
+/**
+ * Runs when popup.html is opened; modifies the HTML to update maps link
+ */
+function initPopup() {
   chrome.storage.sync.get(['urlKey'], function(result) { 
     document.getElementById('link').setAttribute("href", result.urlKey);
   });
 }
 
-function coordHandler() { 
-  determineCoords(serverJSON);
-}
-
-if (location.href.includes("geoguessr")) {
-  setInterval(coordHandler, 5*1000);
-}
-
-if (location.href.includes("popup")) {
-  setURL(destURL);
-}
-
 /**
- * 
- * TODO:
- * Fix open url on first load
- * You rank X in this lobby.
- * Find most recent coordinates, not just last index coords
+ * Calls determineCoords with the endpointURL based on the interval.
  */
+function coordHandler() { 
+  determineCoords(endpointURL);
+}
